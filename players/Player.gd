7e118bea7ed97eec883296fe13e6a277956591a4
吉翔 <jx4554@gmail.com>
2019-04-16 21:38:33 +0800

    Player can directly move to mouse click position

--- players/Player.gd
+++ players/Player.gd
@@ -1,23 +1,17 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 200000
 
-var velocity = Vector2()
-
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
-	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
-	velocity = velocity.normalized() * speed
+var destination_position = Vector2()
+var min_moving_step = 10
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide(velocity)
+	move(destination_position)
 
+func _input(e):
+	if (e is InputEventMouseButton and e.button_index == BUTTON_LEFT and not e.pressed):
+		destination_position = get_global_mouse_position()
 
+func move(to_position):
+	position.x = to_position.x
+	position.y = to_position.y

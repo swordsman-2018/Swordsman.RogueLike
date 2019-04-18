51943e3735d13d1324673838022e5efe416ea6e7
吉翔 <jx4554@gmail.com>
2019-04-18 21:43:25 +0800

    Player can't got out of the wall

--- players/Player.gd
+++ players/Player.gd
@@ -5,6 +5,9 @@ export (int) var speed = 200000
 var destination_position = Vector2()
 var min_moving_step = 10
 
+func _ready():
+	destination_position = position
+
 func _physics_process(delta):
 	move(destination_position)
 
@@ -13,5 +16,6 @@ func _input(e):
 		destination_position = get_global_mouse_position()
 
 func move(to_position):
-	position.x = to_position.x
-	position.y = to_position.y
+	move_and_slide(to_position - position)
+#	position.x = to_position.x
+#	position.y = to_position.y

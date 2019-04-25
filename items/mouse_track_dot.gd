214075e1ad2224220667192eb4ee6ae47134c4b7
吉翔 <jx4554@gmail.com>
2019-04-25 21:24:46 +0800

    Add a mouse track dot

---
+++ items/mouse_track_dot.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+	position = mouse_position
